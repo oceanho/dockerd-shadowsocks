bb5d27db77f27b8f2397a05a5b39511373ef913f
Ocean Ho <ocean@oceanho.com>
2020-05-11 16:11:42 +0800

    Create server.installer.sh

---
+++ script/server.installer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+
+docker_static_dl_link=""
+docker_compose_static_dl_link=""
+
+function install_docker(){
+  echo $#
+}
+
+function install_docker_compose(){
+  echo install docker-compose.
+}
